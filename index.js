aadc26466f615fd26452af99faf9fb1aea9c30d0
Derek Johnson <drk.w.jhnsn@gmail.com>
2018-06-28 07:41:43 -0700

    Implement basic Stripe payment server

---
+++ index.js
@@ -0,0 +1,31 @@
+require('envfile').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.JSON());
+
+app.post('/payment', (req, res) => {
+  charge(req)
+  .then((data) => {
+    res.status(200).send('Payment accepted');
+  })
+  .catch((err) => {
+    res.status(400).send('Payment failed');
+  })
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Stripe server Listening at port ${PORT}`));
+
+function charge(req) {
+  const { stripeToken, amount } = req.body;
+  return stripe.charges.create({
+    amount: parseInt(amount, 10),
+    currency: 'usd',
+    source: stripeToken,
+    description: 'robot burger',
+    metadata: {},
+  });
+}
