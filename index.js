03d4511afcf8124fd500f7f19a964d11f9462de0
Derek Johnson <derek@Dereks-MacBook-Pro.local>
2018-07-11 16:24:24 -0700

    Do stuff to other stuff

--- index.js
+++ index.js
@@ -4,17 +4,55 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const wallets = ['ch_1CmmfWHEqeQ8EqTjH9mHpqPg']; // not setting up a db for this.
+
 app.use(bodyParser.json());
 
-app.post('/payment', (req, res) => {
-  charge(req)
+app.post('/stripe/wallet', (req, res) => {
+  createWallet(req)
   .then((data) => {
-    res.status(200).send('Payment accepted');
-    console.log('Payment accepted')
+    console.log(data)
+    console.log('wallet created');
+    res.status(200).send(data);
+  })
+  .catch((err) => {
+    res.status(400).send(err.message);
+  })
+})
+
+app.get('/stripe/wallet', (req, res) => {
+  console.log(req.query)
+  getWallet(req)
+  .then((response) => {
+    res.status(200).send(response);
+    console.log('Wallet Loaded')
   })
   .catch((err) => {
     console.log(err)
-    res.status(400).send('Payment failed');
+    res.status(400).send(err.message);
+  })
+})
+
+app.post('/wallet/card', (req, res) => {
+  addCard(req)
+  .then((data) => {
+    res.status(200).send(data);
+  })
+  .catch((err) => {
+    res.status(400).send(err.message);
+  })
+})
+
+app.post('/stripe/charge', (req, res) => {
+  charge(req)
+  .then((data) => {
+    res.status(200).send(data);
+    console.log(data)
+    console.log('Payment accepted')
+  })
+  .catch((err) => {
+    console.log(err.message)
+    res.status(400).send(err);
   })
 });
 
@@ -22,13 +60,32 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Stripe server Listening at port ${PORT}`));
 
+function createWallet(req) {
+  return stripe.customers.create({
+    source: req.body.source
+  })
+  .then((data) => {
+    wallets.push(data.id);
+    return data;
+  })
+}
+
+function getWallet(req) {
+  return stripe.customers.retrieve(req.query.customer_id)
+}
+
+function addCard(req) {
+  return stripe.customers.createSource(req.body.walletId, { source: req.body.source })
+}
+
 function charge(req) {
-  const { tokenId, amount } = req.body;
+  const { source, amount, customer } = req.body;
   return stripe.charges.create({
     amount: parseInt(amount, 10),
     currency: 'usd',
-    source: tokenId,
+    source: source,
     description: 'robot burger',
+    customer,
     metadata: {},
   });
 }
