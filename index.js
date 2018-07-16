3645077a52ff9a870c41dc3717d99797ef4a441c
Derek Johnson <derek@Dereks-MacBook-Pro.local>
2018-07-16 14:32:46 -0700

    Refactor

--- index.js
+++ index.js
@@ -3,85 +3,67 @@ const stripe = require('stripe')(process.env.NODE_ENV === 'production' ? process
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const router = express.Router()
 
 app.use(bodyParser.json());
+app.use('/stripe', router);
+app.use(handleError);
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Stripe server Listening at port ${PORT}`));
 
-app.post('/stripe/wallet', (req, res) => {
+router.post('/wallet', (req, res) => {
   createWallet(req)
   .then((data) => {
-    console.log(data)
-    console.log('wallet created');
     res.status(200).send(data);
-  })
-  .catch((err) => {
-    res.status(400).send(err.message);
+    console.log('Wallet created');
   })
 })
 
-app.get('/stripe/wallet/:wallet_id', (req, res) => {
-  console.log(req.params)
+router.get('/wallet/:wallet_id', (req, res) => {
   getWallet(req)
   .then((response) => {
     res.status(200).send(response);
     console.log('Wallet Loaded')
   })
-  .catch((err) => {
-    console.log(err)
-    res.status(400).send(err.message);
-  })
 })
 
-app.delete('/stripe/wallet/:wallet_id/card/:card_id', (req, res) => {
-  console.log(req.params)
+router.delete('/wallet/:wallet_id/card/:card_id', (req, res) => {
   deleteCard(req)
   .then((response) => {
     res.status(200).send(response);
-    console.log('Card removed from wallet')
-  })
-  .catch((err) => {
-    console.log(err)
-    res.status(400).send(err.message);
+    console.log('Card removed from wallet');
   })
 })
 
-app.post('/stripe/wallet/:wallet_id/card', (req, res) => {
+router.post('/wallet/:wallet_id/card', (req, res) => {
   addCard(req)
   .then((data) => {
     res.status(200).send(data);
-    console.log('Card added to wallet')
-  })
-  .catch((err) => {
-    res.status(400).send(err);
+    console.log('Card added to wallet');
   })
 })
 
-app.post('/stripe/charge', (req, res) => {
+router.post('/charge', (req, res) => {
   charge(req)
   .then((data) => {
     res.status(200).send(data);
-    console.log(data)
-    console.log('Payment accepted')
-  })
-  .catch((err) => {
-    console.log(err.message)
-    res.status(400).send(err);
+    console.log('Payment accepted');
   })
 });
 
 function createWallet(req) {
-  return stripe.customers.create({source: req.body.source})
+  return stripe.customers.create({source: req.body.source});
 }
 
 function getWallet(req) {
-  return stripe.customers.retrieve(req.params.wallet_id)
+  return stripe.customers.retrieve(req.params.wallet_id);
 }
 
 function addCard(req) {
-  return stripe.customers.createSource(req.params.wallet_id, { source: req.body.token })
+  return stripe.customers.createSource(req.params.wallet_id, { source: req.body.token });
 }
 
 function deleteCard(req) {
@@ -98,4 +80,9 @@ function charge(req) {
     customer,
     metadata: {}, // store whatever information about purchase you'd like
   });
+}
+
+function handleError(err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send(err)
 }
